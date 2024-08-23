b924dd05ed3db93c0fd3f8a9878e1ac96e8b401c
JoseJG77 <124071581+josegironjg7@users.noreply.github.com>
2024-08-23 12:34:37 -0600

    Create proyecto 1

---
+++ src/main/java/com/example/JG7/SpringBootFundamentalApplication.java
@@ -0,0 +1,13 @@
+package com.example.JG7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFundamentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFundamentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JG7/SpringBootFundamentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JG7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFundamentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
